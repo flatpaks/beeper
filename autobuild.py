53489ac0921e83524864824e756e82967fc06cab
adamthiede <me@adamthiede.com>
2025-03-08 06:41:03 -0600

    update autobuild

--- autobuild.py
+++ autobuild.py
@@ -2,28 +2,47 @@
 
 import subprocess
 import http.client
+import requests
+import hashlib
 
 endpoint="https://api.beeper.com/desktop/download/linux/x64/stable/com.automattic.beeper.desktop"
 
-current="https://beeper-desktop.download.beeper.com/builds/Beeper-4.0.494.AppImage"
+current="4.0.494"
 
-curl_call=subprocess.run(["curl", "-si", endpoint], capture_output=True)
-
-ver=curl_call.stdout
-
+# get headers of the download url to get the "real" version download url
 conn = http.client.HTTPSConnection('api.beeper.com')
 conn.request('GET', '/desktop/download/linux/x64/stable/com.automattic.beeper.desktop')
 response = conn.getresponse()
 loc=response.getheader("Location")
 
-if loc != current:
-    subprocess.run(["sed", "-e", f"'s,{current},{loc},'", "-i", "autobuild.py"])
-else:
-    print("no new version.")
-    exit()
-
+# parse version
 urlarr=loc.split("/")
 fname=urlarr[len(urlarr)-1]
 print(fname)
 version=fname.lower().replace("beeper-", "").replace(".appimage", "")
 print(version)
+
+if version == current:
+    print("no new version.")
+    exit()
+
+# update strings in files
+update_files=["autobuild.py", ".github/workflows/build.yml", "com.beeper.beeper.yaml"]
+for name in update_files:
+    sed_expr=f"sed -e 's,{current},{version},' -i {name}"
+    print(sed_expr)
+    subprocess.run(sed_expr, shell=True)
+
+# update shasum in com.beeper.beeper.yaml
+dl = requests.get(loc)
+open('beeper.appimage', 'wb').write(requests.get(loc).content)
+
+
+sha256_hash = hashlib.sha256()
+with open('beeper.appimage',"rb") as f:
+    for byte_block in iter(lambda: f.read(4096),b""):
+        sha256_hash.update(byte_block)
+    shasum=sha256_hash.hexdigest()
+    sed_expr=f"sed -e 's,sha256: .*,sha256: {shasum},' -i com.beeper.beeper.yaml"
+    subprocess.run(sed_expr, shell=True)
+
