e3ac8780464a93119e46ac4dd129c893f994e9ed
adamthiede <adamthiede@users.noreply.github.com>
2025-08-05 03:44:57 +0000

    autobuild for 4.1.41

--- autobuild.py
+++ autobuild.py
@@ -7,7 +7,7 @@ import hashlib
 
 endpoint="https://api.beeper.com/desktop/download/linux/x64/stable/com.automattic.beeper.desktop"
 
-current="4.1.20"
+current="4.1.41"
 
 # get headers of the download url to get the "real" version download url
 conn = http.client.HTTPSConnection('api.beeper.com')
